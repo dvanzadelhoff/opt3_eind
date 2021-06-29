238c9719169f77883c689690942afe9d2363d0e9
Dennis van Zadelhoff <dvanzadelhoff2412@gmail.com>
2021-06-30 01:40:08 +0200

    fresh start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ProgrammaList p1 = ProgrammaList.getInstance();
 
-        //zorg ervoor dat je Word.toggleBlock kan doen
-        p1.getProgramma(0).toggleBlock();
-        p1.getProgramma(3).toggleBlock();
-
-        p1.showProgramma();
-        p1.showBlockedProgrammas();
-        p1.showProgrammaLocation();
 
     }
 }
